d572b39474c41d1316bc2b9326cea33c4b31a6d4
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2022-05-18 17:33:59 +0800

    Add files via upload

--- js/Managers.js
+++ js/Managers.js
@@ -31,10 +31,10 @@ Managers.Data.ReadJSONFile = function (file, varName, afterFunc)
             
             for (let i = 0; i < fileLength; i++)
             {
-                arrayRequest += `call${i} = () => { Managers.Data.ReadJSONFile("${file[i]}", "${varName[i]}", call${i + 1}); }; `;
+                arrayRequest += `var call${i} = () => { Managers.Data.ReadJSONFile("${file[i]}", "${varName[i]}", call${i + 1}); }; `;
             }
             
-            requestFunc = Function("afterFunc", `${arrayRequest}call${fileLength} = () => { Managers.Data.ReadJSONFile("${file[fileLength]}", "${varName[fileLength]}", afterFunc); }; call0();`);
+            requestFunc = Function("afterFunc", `${arrayRequest} var call${fileLength} = () => { Managers.Data.ReadJSONFile("${file[fileLength]}", "${varName[fileLength]}", afterFunc); }; call0();`);
             break;
     }
     
