21762b41d51a6d1ab45cccc8a2c98b0d7bbb1b80
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2022-08-12 16:18:04 +0800

    Add files via upload

--- js/test/main.js
+++ js/test/main.js
@@ -1,1 +1,10 @@
-
+class testGB extends GameBehavior
+{
+    constructor () { super(); }
+    
+    Start ()
+    {
+        console.log("Start");
+        Application.gl.clearColor(0.4, 0.4, 0.4, 1.0);
+    }
+}
