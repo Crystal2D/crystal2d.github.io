a6a8eb3ded43ae49461900dc4a5527933ffab2b1
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2022-08-12 16:24:09 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-window.onload = () => {
-    BlankEngine.Core.initiateProgram();
-};
+class testGB extends GameBehavior
+{
+    constructor () { super(); }
+    
+    Start ()
+    {
+        console.log("Start");
+        Application.gl.clearColor(0.4, 0.4, 0.4, 1.0);
+    }
+}
