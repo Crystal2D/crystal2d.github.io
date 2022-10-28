8a6c38f54f4c266f679c656b3636adfabb437986
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2022-10-29 02:14:08 +0800

    Add files via upload

--- js/libs/BlankEngine.Core/Scripting/Component.js
+++ js/libs/BlankEngine.Core/Scripting/Component.js
@@ -1,1 +1,38 @@
-
+class Component
+{
+    gameObject = null;
+    
+    BroadcastMessage (method, params, data)
+    {
+        if (!gameObject.activeSelf) return;
+        
+        let components = this.gameObject.components;
+        var args = "";
+        var data = data ?? { };
+        
+        if (params == null || typeof params == "string") args = params;
+        else
+        {
+            for (let i = 0; i < params.length; i++)
+            {
+                args += `params[${i}]`;
+                
+                if (i != params.length - 1) args += ",";
+            }
+        }
+        
+        for (let i = 0; i < components.length; i++)
+        {
+            let validType = components[i] instanceof GameBehavior;
+            
+            if (!components[i].enabled || !validType) continue;
+            
+            eval(`components[i].${method}(${args})`);
+            
+            if (data.clearAfter) eval(`components[i].${method} = function () { }`);
+            
+            components[i].gameObject = this.gameObject;
+            components[i].name = this.name;
+        }
+    }
+}
