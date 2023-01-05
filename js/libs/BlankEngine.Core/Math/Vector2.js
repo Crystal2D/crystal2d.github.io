dda4c50de5ca960ebf5447bfe5c0bf7c3b65e146
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-01-05 11:06:36 +0800

    Add files via upload

--- js/libs/BlankEngine.Core/Math/Vector2.js
+++ js/libs/BlankEngine.Core/Math/Vector2.js
@@ -266,6 +266,15 @@ class Vector2
     }
     
     
+    
+    static Scale (a, b)
+    {
+        if (a == null || b == null) throw BlankEngine.Err(0);
+        
+        return new Vector2(a.x * b.x, a.y * b.y);
+    }
+    
+    
     // Methods
     
     /**
