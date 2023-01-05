8567d5e8ad4a6be17a625c7c9cc7a6380f1e2b33
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-01-05 11:03:51 +0800

    Add files via upload

--- js/libs/BlankEngine.Core/Graphics/Rendering/Camera.js
+++ js/libs/BlankEngine.Core/Graphics/Rendering/Camera.js
@@ -24,25 +24,30 @@ class Camera extends Behavior
     Render ()
     {
         const projM = this.projectionMatrix.matrix;
-        const camM = this.worldToCameraMatrix;
+        let camM = this.worldToCameraMatrix;
         
-        const mPos = new Vector2(-camM.matrix[2][0], -camM.matrix[2][1]);
-        const mRot = 0;
         const mScale = new Vector2(1 / (Application.htmlCanvas.width / (Application.htmlCanvas.height / this.orthographicSize)), -1 / this.orthographicSize);
         
-        const transM = Matrix3x3.TRS(mPos, mRot * Math.PI / 180, mScale);
+        const transM = Matrix3x3.TRS(Vector2.zero, 0, mScale);
         
         for (let iA = 0; iA < SceneManager.GetActiveScene().gameObjects.length; iA++)
         {
+            if (!SceneManager.GetActiveScene().gameObjects[iA].activeSelf) continue;
+            
             const renderers = SceneManager.GetActiveScene().gameObjects[iA].GetComponents("SpriteRenderer");
             
             for (let iB = 0; iB < renderers.length; iB++)
             {
                 let lWM = renderers[iB].gameObject.transform.localToWorldMatrix;
                 
-                lWM.matrix[2][1] *= "-1";
+                lWM.matrix[2][1] *= -1;
+                lWM.matrix[2][0] *= -1;
                 
-                renderers[iB].localSpaceMatrix = Matrix3x3.Multiply(Matrix3x3.Multiply(transM, camM), lWM);
+                let renM = Matrix3x3.Multiply(Matrix3x3.Multiply(transM, camM), lWM);
+                
+                renM.matrix[2][0] *= -1;
+                
+                renderers[iB].localSpaceMatrix = renM;
                 renderers[iB].render();
             }
         }
