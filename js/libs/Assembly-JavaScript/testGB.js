552eeb9725ff0055486a046d183fa28745261950
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-06-24 12:33:53 +0800

    Add files via upload

--- js/libs/Assembly-JavaScript/testGB.js
+++ js/libs/Assembly-JavaScript/testGB.js
@@ -1,5 +1,9 @@
 class testGB extends GameBehavior
 {
+    #input = new Vector2();
+    #zoom = 0;
+    #halt = 0;
+    
     speed = 1;
     
     constructor () { super(); }
@@ -13,12 +17,34 @@ class testGB extends GameBehavior
     
     Update ()
     {
-        const input = new Vector2(
-            +Input.GetKey(KeyCode.ArrowRight) - +Input.GetKey(KeyCode.ArrowLeft),
-            +Input.GetKey(KeyCode.ArrowUp) - +Input.GetKey(KeyCode.ArrowDown)
-        );
+        if (this.#zoom <= 0)
+        {
+            this.#input = new Vector2(
+                +Input.GetKey(KeyCode.ArrowRight) - +Input.GetKey(KeyCode.ArrowLeft),
+                +Input.GetKey(KeyCode.ArrowUp) - +Input.GetKey(KeyCode.ArrowDown)
+            );
+            
+            if (!this.#input.Equals(Vector2.zero) && this.#halt <= 0 && Input.GetKey(KeyCode.Shift)) this.#zoom = 0.125;
+            
+            if (this.#zoom > 0) this.gameObject.components[1].material = Resources.Find("materials/mat_invert");
+        }
+        else
+        {
+            if (this.#zoom - Time.deltaTime <= 0) this.#halt = 2;
+            
+            this.#zoom -= Time.deltaTime;
+        }
         
-        this.transform.position.x += input.x * this.speed * Time.deltaTime;
-        this.transform.position.y += input.y * this.speed * Time.deltaTime;
+        if (this.#halt > 0)
+        {
+            if (this.#halt - Time.deltaTime <= 0) this.gameObject.components[1].material = new Material();
+            
+            this.#halt -= Time.deltaTime;
+        }
+        
+        const speed = this.speed * (this.#zoom > 0 ? 10 : 1);
+       
+        this.transform.position.x += this.#input.x * speed * Time.deltaTime;
+        this.transform.position.y += this.#input.y * speed * Time.deltaTime;
     }
 }
