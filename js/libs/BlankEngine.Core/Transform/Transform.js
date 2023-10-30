f9db3321abc0d458bc0d18617abfa3b850f94b86
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-10-30 14:21:15 +0800

    Add files via upload

--- js/libs/BlankEngine.Core/Transform/Transform.js
+++ js/libs/BlankEngine.Core/Transform/Transform.js
@@ -1,1 +1,27 @@
-
+class Transform extends Component
+{
+    position = new Vector2();
+    rotation = 0;
+    scale = new Vector2();
+    
+    get localToWorldMatrix ()
+    {
+        const output = Matrix3x3.TRS(this.position, 5.555555555555556e-3 * this.rotation * Math.PI, this.scale);
+        
+        return output;
+    }
+    
+    get worldToLocalMatrix ()
+    {
+        return localToWorldMatrix.inverse;
+    }
+    
+    constructor (position, rotation, scale)
+    {
+        super();
+        
+        this.position = position ?? Vector2.zero;
+        this.rotation = rotation ?? 0;
+        this.scale = scale ?? Vector2.one;
+    }
+}
