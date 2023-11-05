c3824929e83eaa8db23716c846a359ce0ed7b0e4
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-11-05 22:07:32 +0800

    Add files via upload

--- js/libs/BlankEngine.Core/Transform/Transform.js
+++ js/libs/BlankEngine.Core/Transform/Transform.js
@@ -1,7 +1,8 @@
 class Transform extends Component
 {
-    position = new Vector2();
     rotation = 0;
+    
+    position = new Vector2();
     scale = new Vector2();
     
     get localToWorldMatrix ()
