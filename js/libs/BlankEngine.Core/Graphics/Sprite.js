e739852266e0d3c744c37283797bff58ea8b6889
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-11-05 22:03:11 +0800

    Add files via upload

--- js/libs/BlankEngine.Core/Graphics/Sprite.js
+++ js/libs/BlankEngine.Core/Graphics/Sprite.js
@@ -1,11 +1,38 @@
 class Sprite
 {
+    pivot = new Vector2(0.5, 0.5);
+    
     texture = null;
     rect = null;
+    
+    get vertices ()
+    {
+        const rect = new Rect(
+            this.rect.x, 
+            this.rect.y,
+            this.rect.width / this.texture.width,
+            this.rect.height / this.texture.height
+        );
+        
+        return [
+            new Vector2(rect.x, rect.y),
+            new Vector2(rect.xMax, rect.y),
+            new Vector2(rect.x, rect.yMax),
+            new Vector2(rect.xMax, rect.yMax)
+        ];
+    }
+    
+    get triangles ()
+    {
+        return [
+            0, 2, 1,
+            2, 3, 1
+        ];
+    }
     
     constructor (texture, rect)
     {
         this.texture = texture;
-        this.rect = rect ?? new Rect();
+        this.rect = rect ?? new Rect(0, 0, texture.width, texture.height);
     }
 }
--- js/libs/BlankEngine.Core/Graphics/Texture.js
+++ js/libs/BlankEngine.Core/Graphics/Texture.js
@@ -1,12 +1,15 @@
 class Texture
 {
     #loaded = false;
+    #width = 0;
+    #height = 0;
+    #wrap = 0;
+    #filter = 0;
     #src = "";
     
     #img = null;
-    
-    wrapMode = 0;
-    filterMode = 0;
+    #gl = null;
+    #texture = null;
     
     get isLoaded ()
     {
@@ -20,12 +23,73 @@ class Texture
     
     get width ()
     {
-        return this.#img.width;
+        return this.#width;
     }
     
     get height ()
     {
-        return this.#img.height;
+        return this.#height;
+    }
+    
+    get wrapMode ()
+    {
+        return this.#wrap;
+    }
+    
+    set wrapMode (value)
+    {
+        this.#wrap = value;
+        
+        const gl = this.#gl;
+        
+        let mode = null;
+        
+        switch (value)
+        {
+            case 0:
+                mode = gl.CLAMP_TO_EDGE;
+                break;
+            case 1:
+                mode = gl.REPEAT;
+                break;
+            case 2:
+                mode = gl.MIRRORED_REPEAT;
+                break;
+        }
+        
+        gl.bindTexture(gl.TEXTURE_2D, this.#texture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, mode);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, mode);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+    
+    get filterMode ()
+    {
+        return this.#filter;
+    }
+    
+    set filterMode (value)
+    {
+        this.#filter = value;
+        
+        const gl = this.#gl;
+        
+        let mode = null;
+        
+        switch (value)
+        {
+            case 0:
+                mode = gl.LINEAR;
+                break;
+            case 1:
+                mode = gl.NEAREST;
+                break;
+        }
+        
+        gl.bindTexture(gl.TEXTURE_2D, this.#texture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, mode);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, mode);
+        gl.bindTexture(gl.TEXTURE_2D, null);
     }
     
     constructor (src)
@@ -33,7 +97,16 @@ class Texture
         this.#img = new Image();
         this.#img.sprite = this;
         
+        this.#gl = Application.gl;
+        
+        this.#texture = this.#gl.createTexture();
+        
         this.#src = src;
+    }
+    
+    GetNativeTexture ()
+    {
+        return this.#texture;
     }
     
     async Load ()
@@ -43,6 +116,15 @@ class Texture
         this.#img.src = `img/${this.#src}`;
         
         await new Promise(resolve => this.#img.onload = resolve);
+        
+        this.#width = this.#img.width;
+        this.#height = this.#img.height;
+        
+        const gl = this.#gl;
+        
+        gl.bindTexture(gl.TEXTURE_2D, this.#texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.#img);
+        gl.bindTexture(gl.TEXTURE_2D, null);
         
         this.#loaded = true;
     }
