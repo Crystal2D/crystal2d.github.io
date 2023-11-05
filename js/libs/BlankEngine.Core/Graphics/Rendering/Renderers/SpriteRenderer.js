9e62a324fad955f0c17d7e553a765edc436865f3
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-11-05 22:01:07 +0800

    Add files via upload

--- js/libs/BlankEngine.Core/Graphics/Rendering/Renderers/SpriteRenderer.js
+++ js/libs/BlankEngine.Core/Graphics/Rendering/Renderers/SpriteRenderer.js
@@ -1,19 +1,18 @@
 class SpriteRenderer extends Component
 {
     #loaded = false;
+    #uMatrixID = 0;
+    #geometryBufferID = 0;
+    #textureBufferID = 0;
+    #aVertexPosID = 0;
+    #aTexturePosID = 0;
+    
     #sprite = null;
     #spriteOld = null;
     #material = null;
     #materialOld = null;
-    #texture = null;
-    #textureBuffer = null;
-    #geometryBuffer = null;
-    #uSampler = null;
-    #uLocalMatrix = null;
-    #uVertexPositionOffset = null;
-    #aVertexPosition = null;
-    #aTexturePosition = null;
     
+    color = Color.white;
     localSpaceMatrix = new Matrix3x3();
     
     get isLoaded ()
@@ -66,80 +65,23 @@ class SpriteRenderer extends Component
     
     Load ()
     {
-        const texture = this.#sprite.texture;
-        
         const gl = this.#material.gl;
         
-        const rect = this.#sprite.rect;
-        const x = rect.x;
-        const y = rect.y;
-        const width = rect.width;
-        const height = rect.height;
-        const xMin = rect.xMin;
-        const yMin = rect.yMin;
-        const rectArray = [
-            x + xMin, y + yMin,
-            x + width, y + yMin,
-            x + width, y + height,
-            x + xMin, y + yMin,
-            x + xMin, y + height,
-            x + width, y + height
-        ];
+        this.#material.SetSampler2D("uSampler", 0);
         
-        let filterMode;
-        let wrapMode;
-        
-        switch (texture.filterMode)
-        {
-            case 1:
-                filterMode = gl.NEAREST;
-                break;
-            default:
-                filterMode = gl.LINEAR;
-        }
-        
-        switch (texture.wrapMode)
-        {
-            case 1:
-                wrapMode = gl.REPEAT;
-                break;
-            case 2:
-                wrapMode = gl.MIRRORED_REPEAT;
-                break;
-            default:
-                wrapMode = gl.CLAMP_TO_EDGE;
-        }
-        
-        gl.useProgram(this.#material.program);
-        
-        this.#texture = gl.createTexture();
-        this.#textureBuffer = gl.createBuffer();
-        this.#geometryBuffer = gl.createBuffer();
-        
-        this.#uSampler = this.#material.getUniformLocation("uSampler");
-        this.#uLocalMatrix = this.#material.getUniformLocation("uLocalMatrix");
-        this.#uVertexPositionOffset = this.#material.getUniformLocation("uVertexPositionOffset");
-        this.#aVertexPosition = this.#material.getAttribLocation("aVertexPosition");
-        this.#aTexturePosition = this.#material.getAttribLocation("aTexturePosition");
-        
-        gl.bindTexture(gl.TEXTURE_2D, this.#texture);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, wrapMode);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, wrapMode);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, filterMode);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, filterMode);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.img);
-        gl.bindTexture(gl.TEXTURE_2D, null);
-        
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.#geometryBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(rectArray), gl.STATIC_DRAW);
-        
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.#textureBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(rectArray), gl.STATIC_DRAW);
-        
-        gl.useProgram(null);
+        const geometryBuffer = this.#material.AddBuffer("geometry", null, 2);
+        const textureBuffer = this.#material.AddBuffer("texture", null, 2);
         
         this.#spriteOld = this.#sprite;
         this.#materialOld = this.#material;
+        
+        this.#uMatrixID = this.material.GetPropertyNameID("uMatrix");
+        
+        this.#geometryBufferID = geometryBuffer;
+        this.#textureBufferID = textureBuffer;
+        
+        this.#aVertexPosID = this.material.GetAttributeNameID("aVertexPos");
+        this.#aTexturePosID = this.material.GetAttributeNameID("aTexturePos");
         
         this.#loaded = true;
     }
@@ -148,18 +90,39 @@ class SpriteRenderer extends Component
     {
         if (!this.#loaded || !this.gameObject.activeSelf) return;
         
-        const gl = this.#materialOld.gl;
+        const gl = this.material.gl;
         
-        const x = this.#sprite.texture.width;
-        const y = this.#sprite.texture.height;
+        const vertices = this.sprite.vertices;
+        const tris = this.sprite.triangles;
         
-        const scale = x > y ? new Vector2(1, y / x) : new Vector2(x / y, 1);
+        let rectArray = [];
+        
+        for (let i = 0; i < tris.length; i++)
+        {
+            const vertex = vertices[tris[i]];
+            const index = i * 2;
+            
+            rectArray[index] = vertex.x;
+            rectArray[index + 1] = vertex.y;
+        }
+        
+        const texX = this.sprite.texture.width;
+        const texY = this.sprite.texture.height;
+        
+        const scale = texX > texY ? new Vector2(1, texY / texX) : new Vector2(texX / texY, 1);
         
         const localMatrix = Matrix3x3.Multiply(
             this.localSpaceMatrix,
-            Matrix3x3.Scale(scale)
+            Matrix3x3.TRS(
+                Vector2.Scale(this.sprite.pivot, -1),
+                0,
+                scale
+            )
         );
-        const appliedMatrix = [
+        
+        this.material.color = this.color;
+        
+        this.material.SetMatrix(this.#uMatrixID,
             localMatrix.matrix[0][0],
             localMatrix.matrix[0][1],
             localMatrix.matrix[0][2],
@@ -169,30 +132,20 @@ class SpriteRenderer extends Component
             localMatrix.matrix[2][0],
             localMatrix.matrix[2][1],
             localMatrix.matrix[2][2]
-        ];
+        );
         
-        gl.useProgram(this.#materialOld.program);
+        this.material.SetBuffer(this.#geometryBufferID, rectArray);
+        this.material.SetBuffer(this.#textureBufferID, rectArray);
+        
+        this.material.SetAttribute(this.#aVertexPosID, this.#geometryBufferID);
+        this.material.SetAttribute(this.#aTexturePosID, this.#textureBufferID);
+        
+        gl.useProgram(this.material.program);
         
         gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, this.#texture);
+        gl.bindTexture(gl.TEXTURE_2D, this.sprite.texture.GetNativeTexture());
         
-        gl.uniform1i(this.#uSampler, 0);
-        
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.#geometryBuffer);
-        gl.enableVertexAttribArray(this.#aVertexPosition);
-        gl.vertexAttribPointer(this.#aVertexPosition, 2, gl.FLOAT, false, 0, 0);
-        
-        gl.uniformMatrix3fv(this.#uLocalMatrix, false, new Float32Array(appliedMatrix));
-        
-        const center = this.#spriteOld.rect.center;
-        
-        gl.uniform2fv(this.#uVertexPositionOffset, new Float32Array([-0.5 * (1 + center.x), -0.5 * (1 + center.y)]))
-        
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.#textureBuffer);
-        gl.enableVertexAttribArray(this.#aTexturePosition);
-        gl.vertexAttribPointer(this.#aTexturePosition, 2, gl.FLOAT, false, 0, 0);
-        
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, tris.length);
         
         gl.useProgram(null);
     }
