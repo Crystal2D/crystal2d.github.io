5ce4aa1e308d9229ed65bc6b51d80c30cd103229
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-11-09 20:10:52 +0800

    Add files via upload

--- js/libs/BlankEngine.Core/Scripting/Component.js
+++ js/libs/BlankEngine.Core/Scripting/Component.js
@@ -14,34 +14,6 @@ class Component
     
     BroadcastMessage (method, params, data)
     {
-        if (!gameObject.activeSelf) return;
-        
-        const components = this.gameObject.components;
-        
-        let args = "";
-        let dat = data ?? { };
-        
-        if (Array.isArray(params))
-        {
-            for (let i = 0; i < params.length; i++)
-            {
-                args += `params[${i}]`;
-                
-                if (i !== params.length - 1) args += ",";
-            }
-        }
-        else args = params;
-        
-        for (let i = 0; i < components.length; i++)
-        {
-            if (!components[i].enabled || !(components[i] instanceof GameBehavior)) continue;
-            
-            eval(`components[i].${method}(${args})`);
-            
-            if (dat.clearAfter) eval(`components[i].${method} = () => { }`);
-            
-            components[i].gameObject = this.gameObject;
-            components[i].name = this.name;
-        }
+        this.gameObject.BroadcastMessage(method, params, data);
     }
 }
