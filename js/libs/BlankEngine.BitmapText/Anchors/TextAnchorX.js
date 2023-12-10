45757c41a164a62743cc7d470c7edd4dab4ebcf5
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-12-10 14:33:53 +0800

    Add files via upload

--- js/libs/BlankEngine.BitmapText/Anchors/TextAnchorX.js
+++ js/libs/BlankEngine.BitmapText/Anchors/TextAnchorX.js
@@ -1,1 +1,6 @@
-
+class TextAnchorX
+{
+    static get Left () { return 0; }
+    static get Center () { return 1; }
+    static get Right () { return 2; }
+}
---
+++ js/libs/BlankEngine.BitmapText/Anchors/TextAnchorY.js
@@ -0,0 +1,6 @@
+class TextAnchorY
+{
+    static get Top () { return 0; }
+    static get Middle () { return 1; }
+    static get Bottom () { return 2; }
+}
