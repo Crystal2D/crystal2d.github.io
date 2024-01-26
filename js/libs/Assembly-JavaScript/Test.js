db8f678deb84e6fb38045e061896ac82ee7aa703
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2024-01-26 16:50:13 +0800

    Add files via upload

--- js/libs/Assembly-JavaScript/Test.js
+++ js/libs/Assembly-JavaScript/Test.js
@@ -1,21 +1,99 @@
-class Test extends GameBehavior
+class Test extends Viewport
 {
-    constructor () { super(); Application.gl.clearColor(0.6, 0.6, 0.6, 1); }
+    #iter = 1000;
+    #area = 100;
+    #sizeMin = 1;
+    #sizeMax = 2;
+    #speed = 4;
+    #scale = 0;
+    #scaleDelta = 0
+    #scaleSpeed = 10;
+    #scaleMin = 0;
+    #scaleMax = 100;
+    
+    #input = Vector2.zero;
+    
+    #camera = null;
+    
+    constructor () { super(); }
+    
+    #Rand (min, max)
+    {
+        return Math.random() * (max - min) + min;
+    }
 
+    Start ()
+    {
+        this.#camera = this.GetComponent("Camera");
+        
+        this.#scaleMin = this.#camera.orthographicSize;
+        this.#scale = this.#scaleMin;
+        
+        const sprite = Resources.Find("sprites/square").sprites[0];
+        
+        let objs = [];
+        
+        for (let i = 1; i <= this.#iter; i++)
+        {
+            const renderer = new SpriteRenderer(sprite.Duplicate());
+            
+            renderer.color = new Color(
+                Math.random(),
+                Math.random(),
+                Math.random()
+            );
+            
+            const transform = new Transform();
+            
+            transform.position = new Vector2(
+                this.#Rand(0, this.#area),
+                this.#Rand(0, -this.#area)
+            );
+            transform.scale = new Vector2(
+                this.#Rand(this.#sizeMin, this.#sizeMax),
+                this.#Rand(this.#sizeMin, this.#sizeMax)
+            )
+            
+            const obj = new GameObject(
+                "",
+                [renderer],
+                true,
+                transform,
+                i
+            );
+            
+            if (i === 1) objs[0] = obj;
+            else objs.push(obj);
+        }
+        
+        SceneManager.GetActiveScene().gameObjects.push(...objs);
+    }
+    
+    FixedUpdate ()
+    {
+        if (this.#scaleDelta !== 0)
+        {
+            this.#scale = Math.Clamp(
+                this.#scale + this.#scaleDelta * this.#scaleSpeed * Time.fixedDeltaTime,
+                this.#scaleMin,
+                this.#scaleMax
+            );
+            
+            this.#camera.orthographicSize = this.#scale;
+        }
+        
+        const movement = Vector2.Scale(this.#input, (this.#scale / this.#scaleMin) * this.#speed * Time.fixedDeltaTime);
+       
+        this.transform.position = Vector2.Add(this.transform.position, movement);
+    }
+    
     Update ()
     {
-        let points = [];
-
-        for (let i = 2; i <= 6; i++) points[i - 2] = GameObject.FindByID(i).transform;
-
-        const bounds = this.GetComponent("Text").bounds;
-        const min = bounds.min;
-        const max = bounds.max;
-
-        points[0].position = bounds.center;
-        points[1].position = min;
-        points[2].position = new Vector2(max.x, min.y);
-        points[3].position = new Vector2(min.x, max.y);
-        points[4].position = max;
+        this.#input = new Vector2(
+            +Input.GetKey(KeyCode.ArrowRight) - +Input.GetKey(KeyCode.ArrowLeft),
+            +Input.GetKey(KeyCode.ArrowUp) - +Input.GetKey(KeyCode.ArrowDown)
+        );
+        
+        this.#scaleDelta = +Input.GetKey(KeyCode.Z) - +Input.GetKey(KeyCode.X);
     }
 }
