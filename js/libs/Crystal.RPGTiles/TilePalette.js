39b17d5bcc2989675367467ecd9529d229be1f10
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2024-12-19 22:11:54 +0800

    e

---
+++ js/libs/Crystal.RPGTiles/TilePalette.js
@@ -0,0 +1,61 @@
+class TilePalette
+{
+    static #loaded = false;
+
+    static #unloadedPal = [];
+    static #palettes = [];
+
+    name = "";
+    sprites = [];
+
+    static get isLoaded ()
+    {
+        return this.#loaded;
+    }
+
+    static async Set ()
+    {
+        if (this.#loaded) return;
+
+        const dataRequest = await fetch("data/tilepalettes.json");
+        this.#unloadedPal = await dataRequest.json();
+
+        this.#loaded = true;
+    }
+
+    static async Load (name)
+    {
+        const data = this.#unloadedPal.find(item => item.name === name);
+
+        const obj = new TilePalette();
+        obj.name = name;
+
+        for (let i = 0; i < data.textures.length; i++)
+        {
+            if (data.textures.sprites?.length === 0) continue;
+
+            const texture = Resources.Find(data.textures[i].src);
+
+            obj.sprites.push(...data.textures[i].sprites.map(item => {
+                return {
+                    id: item.id,
+                    sprite: texture.sprites[item.index].Duplicate()
+                };
+            }));
+        }
+
+        this.#palettes.push(obj);
+    }
+
+    static async UnloadAll ()
+    {
+        this.#palettes = [];
+    }
+    
+    static Find (name)
+    {
+        return this.#palettes.find(item => item.name === name);
+    }
+}
+
+TilePalette.Set();
